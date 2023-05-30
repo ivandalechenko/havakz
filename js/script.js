b08cf274f4a04902cdbe599ca94d30546f3929d8
Иван Далеченко <ivandalechenko@gmail.com>
2023-05-30 22:52:22 +0300

    added lang scaner

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var language = window.navigator ? (window.navigator.language ||
+    window.navigator.systemLanguage ||
+    window.navigator.userLanguage) : "ru";
+language = language.substr(0, 2).toLowerCase();
+
+console.log(language);
+document.getElementById("sum4").innerHTML = language;
